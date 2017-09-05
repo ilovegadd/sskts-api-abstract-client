c2b4258fa9472ce27e58183b2d893088f280d2a1
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-09-05 00:06:46 +0000

    # SSKTS-580 - set this package an abstract api client. - export factory.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,7 @@
  * Sasaki API Service Library for Javascript
  * @ignore
  */
+import * as factory from '@motionpicture/sskts-factory';
 import { IOptions } from './service';
 import { EventService } from './service/event';
 import { OrderService } from './service/order';
@@ -10,72 +11,75 @@ import { PersonService } from './service/person';
 import { PlaceService } from './service/place';
 import { PlaceOrderTransactionService } from './service/transaction/placeOrder';
 import * as transporters from './transporters';
+export import factory = factory;
 export import transporters = transporters;
-/**
- * event service
- * @class
- */
-export declare class Event extends EventService {
-}
-/**
- * order service
- * @class
- */
-export declare class Order extends OrderService {
-}
-/**
- * organization service
- * @class
- */
-export declare class Organization extends OrganizationService {
-}
-/**
- * person service
- * @class
- */
-export declare class Person extends PersonService {
-}
-/**
- * place service
- * @class
- */
-export declare class Place extends PlaceService {
-}
-/**
- * event service
- * @param {IOptions} options service configurations
- */
-export declare function event(options: IOptions): EventService;
-/**
- * order service
- * @param {IOptions} options service configurations
- */
-export declare function order(options: IOptions): OrderService;
-/**
- * organization service
- * @param {IOptions} options service configurations
- */
-export declare function organization(options: IOptions): OrganizationService;
-/**
- * person service
- * @param {IOptions} options service configurations
- */
-export declare function person(options: IOptions): PersonService;
-/**
- * place service
- * @param {IOptions} options service configurations
- */
-export declare function place(options: IOptions): PlaceService;
-export declare namespace transaction {
+export declare namespace service {
     /**
-     * placeOrder transaction service
+     * event service
      * @class
      */
-    class PlaceOrder extends PlaceOrderTransactionService {
+    class Event extends EventService {
     }
     /**
-     * placeOrder transaction service
+     * order service
+     * @class
+     */
+    class Order extends OrderService {
+    }
+    /**
+     * organization service
+     * @class
+     */
+    class Organization extends OrganizationService {
+    }
+    /**
+     * person service
+     * @class
+     */
+    class Person extends PersonService {
+    }
+    /**
+     * place service
+     * @class
+     */
+    class Place extends PlaceService {
+    }
+    /**
+     * event service
      * @param {IOptions} options service configurations
      */
-    function placeOrder(options: IOptions): PlaceOrderTransactionService;
+    function event(options: IOptions): EventService;
+    /**
+     * order service
+     * @param {IOptions} options service configurations
+     */
+    function order(options: IOptions): OrderService;
+    /**
+     * organization service
+     * @param {IOptions} options service configurations
+     */
+    function organization(options: IOptions): OrganizationService;
+    /**
+     * person service
+     * @param {IOptions} options service configurations
+     */
+    function person(options: IOptions): PersonService;
+    /**
+     * place service
+     * @param {IOptions} options service configurations
+     */
+    function place(options: IOptions): PlaceService;
+    namespace transaction {
+        /**
+         * placeOrder transaction service
+         * @class
+         */
+        class PlaceOrder extends PlaceOrderTransactionService {
+        }
+        /**
+         * placeOrder transaction service
+         * @param {IOptions} options service configurations
+         */
+        function placeOrder(options: IOptions): PlaceOrderTransactionService;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@
  * @ignore
  */
 
+import * as factory from '@motionpicture/sskts-factory';
+
 import { IOptions } from './service';
 import { EventService } from './service/event';
 import { OrderService } from './service/order';
@@ -14,80 +16,83 @@ import { PlaceService } from './service/place';
 import { PlaceOrderTransactionService } from './service/transaction/placeOrder';
 import * as transporters from './transporters';
 
+export import factory = factory;
 export import transporters = transporters;
 
-/**
- * event service
- * @class
- */
-export class Event extends EventService { }
-/**
- * order service
- * @class
- */
-export class Order extends OrderService { }
-/**
- * organization service
- * @class
- */
-export class Organization extends OrganizationService { }
-/**
- * person service
- * @class
- */
-export class Person extends PersonService { }
-/**
- * place service
- * @class
- */
-export class Place extends PlaceService { }
-/**
- * event service
- * @param {IOptions} options service configurations
- */
-export function event(options: IOptions) {
-    return new EventService(options);
-}
-/**
- * order service
- * @param {IOptions} options service configurations
- */
-export function order(options: IOptions) {
-    return new OrderService(options);
-}
-/**
- * organization service
- * @param {IOptions} options service configurations
- */
-export function organization(options: IOptions) {
-    return new OrganizationService(options);
-}
-/**
- * person service
- * @param {IOptions} options service configurations
- */
-export function person(options: IOptions) {
-    return new PersonService(options);
-}
-/**
- * place service
- * @param {IOptions} options service configurations
- */
-export function place(options: IOptions) {
-    return new PlaceService(options);
-}
-
-export namespace transaction {
+export namespace service {
     /**
-     * placeOrder transaction service
+     * event service
      * @class
      */
-    export class PlaceOrder extends PlaceOrderTransactionService { }
+    export class Event extends EventService { }
     /**
-     * placeOrder transaction service
+     * order service
+     * @class
+     */
+    export class Order extends OrderService { }
+    /**
+     * organization service
+     * @class
+     */
+    export class Organization extends OrganizationService { }
+    /**
+     * person service
+     * @class
+     */
+    export class Person extends PersonService { }
+    /**
+     * place service
+     * @class
+     */
+    export class Place extends PlaceService { }
+    /**
+     * event service
      * @param {IOptions} options service configurations
      */
-    export function placeOrder(options: IOptions) {
-        return new PlaceOrderTransactionService(options);
+    export function event(options: IOptions) {
+        return new EventService(options);
+    }
+    /**
+     * order service
+     * @param {IOptions} options service configurations
+     */
+    export function order(options: IOptions) {
+        return new OrderService(options);
+    }
+    /**
+     * organization service
+     * @param {IOptions} options service configurations
+     */
+    export function organization(options: IOptions) {
+        return new OrganizationService(options);
+    }
+    /**
+     * person service
+     * @param {IOptions} options service configurations
+     */
+    export function person(options: IOptions) {
+        return new PersonService(options);
+    }
+    /**
+     * place service
+     * @param {IOptions} options service configurations
+     */
+    export function place(options: IOptions) {
+        return new PlaceService(options);
+    }
+
+    export namespace transaction {
+        /**
+         * placeOrder transaction service
+         * @class
+         */
+        export class PlaceOrder extends PlaceOrderTransactionService { }
+        /**
+         * placeOrder transaction service
+         * @param {IOptions} options service configurations
+         */
+        export function placeOrder(options: IOptions) {
+            return new PlaceOrderTransactionService(options);
+        }
     }
 }
